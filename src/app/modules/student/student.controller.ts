0de650298c241aaf3adb20208269f1502ae7739f
momenur <momenurislam6@gmail.com>
2023-11-23 08:02:22 +0600

    added controller, service, model, interface, route

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+const createStudent = (req: Request, res: Response) => {
+  const student = req.body;
+  //will call service function to send this data
+  //sent Response
+};
