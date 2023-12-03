79e8c9a87f2adbb6d08070e3466a8df6238846af
momenur <momenurislam6@gmail.com>
2023-12-03 18:06:15 +0600

    added model and schema

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,29 +1,34 @@
-// import { Schema } from "mongoose";
-// import { TUser } from "./user.interface";
+import { Schema } from 'mongoose';
+import { TUser } from './user.interface';
 
-// const userSchema : new Schema<TUser> ({
-//     id: {
-//         type: String,
-//         required: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     needsPasswordChange: {
-//         type: Boolean,
-//         default: true
-//     },
-//     role: {
-//         type: String,
-//         enum: ['student', 'admin', 'faculty']
-//     },
-//     status: {
-//         type: String,
-//         enum: ['in-progress', 'blocked']
-//     },
-//     isDeleted: {
-//         type: Boolean,
-//         default: false
-//     }
-// });
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'faculty', 'admin'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
