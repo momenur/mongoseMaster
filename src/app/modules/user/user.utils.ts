4dd0fd041e695bf6bab2c124fd6e6052031fe1e5
momenur <momenurislam6@gmail.com>
2023-12-14 23:00:08 +0600

    fixed student id bugs

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -12,13 +12,26 @@ const findLastStudentId = async () => {
   )
     .sort({ createdAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // Year semesterCode 4 digit Number
 export const generateStudentId = async (payLoad: TAcademicSemester) => {
   // At the First Time
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payLoad.code;
+  const currentYear = payLoad.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payLoad.year}${payLoad.code}${incrementId}`;
   return incrementId;
