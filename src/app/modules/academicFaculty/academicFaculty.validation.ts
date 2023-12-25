336becd8e0ff275866e0ea93a3e33bcba464ddde
momenur <momenurislam6@gmail.com>
2023-12-25 21:27:59 +0600

    academic faculty not working

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,14 +1,18 @@
 import { z } from 'zod';
 
 const createAcademicValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty Must be String type',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty Must be String type',
+    }),
   }),
 });
 
 const updateAcademicValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty Must be String type',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty Must be String type',
+    }),
   }),
 });
 
