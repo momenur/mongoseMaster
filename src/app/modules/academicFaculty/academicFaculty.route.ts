df8d5bfe0f203474fa3bff6c9fcebb959cb0f29b
momenur <momenurislam6@gmail.com>
2023-12-26 22:40:17 +0600

    academic depertment start and added interface

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -6,7 +6,7 @@ import { AcademicFacultyControllers } from './academicFaculty.controller';
 const router = express.Router();
 
 router.post(
-  'create-academic-faculty',
+  '/create-academic-faculty',
   validateRequest(AcademicFacultyValidation.createAcademicValidationSchema),
   AcademicFacultyControllers.createAcademicFaculty,
 );
